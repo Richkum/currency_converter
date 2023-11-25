a9e53ee03983115c54479742fa760a718f9f02b4
Richkum <kumatum2@gmail.com>
2023-11-25 02:19:30 +0100

    working on the display and functionalities

--- api.js
+++ api.js
@@ -1,22 +1,22 @@
-async function fetchData() {
-  try {
-    const response = await fetch("https://api.example.com/data");
+// async function fetchData() {
+//   try {
+//     const response = await fetch("https://api.example.com/data");
 
-    // Check if the request was successful (status code 200-299)
-    if (!response.ok) {
-      throw new Error(`Network response was not ok: ${response.statusText}`);
-    }
+//     // Check if the request was successful (status code 200-299)
+//     if (!response.ok) {
+//       throw new Error(`Network response was not ok: ${response.statusText}`);
+//     }
 
-    // Parse the JSON data in the response
-    const data = await response.json();
+//     // Parse the JSON data in the response
+//     const data = await response.json();
 
-    // Handle the data
-    console.log(data);
-  } catch (error) {
-    // Handle errors
-    console.error("Error:", error);
-  }
-}
+//     // Handle the data
+//     console.log(data);
+//   } catch (error) {
+//     // Handle errors
+//     console.error("Error:", error);
+//   }
+// }
 
-// Call the async function
-export default fetchData;
+// // Call the async function
+// export default fetchData;
